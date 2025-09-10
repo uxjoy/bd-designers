b94bc065022a67f7e64ba69198abc14e4fa6181f
Joy210 <sohanoor404@gmail.com>
2025-09-10 15:28:04 +0600

    update

--- components/users/userCard.tsx
+++ components/users/userCard.tsx
@@ -10,32 +10,34 @@ interface User {
 
 const UserCard = ({ user }: any) => {
   return (
-    <div className="user-card relative bg-white px-5 py-10 flex flex-col items-center rounded-3xl text-center card-shadow">
-      <div className="user-avatar h-16 w-16 flex items-center justify-center bg-slate-100 rounded-full text-xl font-bold mb-4">
+    <div className="user-card relative bg-white p-4 py-6 sm:px-5 sm:py-10 flex flex-row sm:flex-col gap-3 sm:items-center rounded-2xl sm:rounded-3xl sm:text-center card-shadow">
+      <div className="user-avatar h-12 w-12 inline-flex items-center justify-center bg-slate-100 rounded-full text-xl font-bold mb-4">
         {user.name.charAt(0)}
       </div>
 
-      <h2 className="text-lg font-semibold">{user.name}</h2>
+      <div className="flex flex-col mt-0.5 flex-1">
+        <h2 className="text-lg font-medium">{user.name}</h2>
 
-      {user.designation && (
-        <p className="text-sm">
-          {user.designation} @ {user.company}
-        </p>
-      )}
+        {user.designation && (
+          <p className="text-sm">
+            {user.designation} @ {user.company}
+          </p>
+        )}
 
-      {/* <span className="pill h-6 px-2 text-center absolute top-2 right-2 text-xs uppercase bg-slate-950 rounded flex items-center justify-center text-white/70 font-medium">
+        {/* <span className="pill h-6 px-2 text-center absolute top-2 right-2 text-xs uppercase bg-slate-950 rounded flex items-center justify-center text-white/70 font-medium">
         {user.followers}
       </span> */}
 
-      {/* <p className="text-sm">{user.company} @</p> */}
+        {/* <p className="text-sm">{user.company} @</p> */}
 
-      <Link
-        href={user.linkedin}
-        target="_blank"
-        className="text-blue-500 mt-4 text-sm uppercase"
-      >
-        View Profile
-      </Link>
+        <Link
+          href={user.linkedin}
+          target="_blank"
+          className="text-blue-500 mt-4 text-xs sm:text-sm uppercase"
+        >
+          View Profile
+        </Link>
+      </div>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,30 @@ import {
   RiBuildingFill,
   RiGroupFill,
   RiUserLocationFill,
-  RiUserLocationLine,
 } from "@remixicon/react";
+import BackToTop from "../../components/BackToTop";
 import UserCard from "../../components/users/userCard";
 import { linkedinUsers } from "./user";
-import BackToTop from "../../components/BackToTop";
 
 export default function Home() {
   return (
     <div className="font-sans">
       <BackToTop />
       <section className="header-section p-2">
-        <div className="bg-zinc-900 text-white p-4 min-h-[550px] flex flex-col justify-center items-center text-center gap-5 rounded-4xl">
-          <div className="bg-white/10 font-medium py-1 px-3 rounded-full border border-white/10 flex items-center gap-1">
+        <div className="bg-zinc-900 text-white p-3 md:p-4 min-h-[400px] md:min-h-[550px] flex flex-col justify-center items-center text-center gap-3 md:gap-5 rounded-3xl md:rounded-4xl">
+          <div className="bg-white/10 font-medium text-sm py-1 px-3 rounded-full border border-white/10 flex items-center gap-1">
             {" "}
-            <RiUserLocationFill size={18} />
+            <RiUserLocationFill size={16} />
             Bangladesh{" "}
           </div>
 
-          <h1 className="text-7xl font-semibold max-w-4xl">
+          <h1 className="text-4xl md:text-7xl font-bold md:font-semibold md:max-w-4xl">
             {" "}
             Bangladeshi <br />{" "}
             <span className="text-white/70">Design Community</span>
           </h1>
-          <p className="text-xl max-w-xl text-white/80">
+
+          <p className="md:text-xl sm:max-w-xl text-white/80">
             Discover and connect with talented UX/UI designers from Bangladesh.
             Building the future of digital experiences.
           </p>
@@ -58,7 +58,7 @@ export default function Home() {
 
       <section className="users">
         <div className="container mx-auto px-4 py-10">
-          <div className="user-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div className="user-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-4">
             {linkedinUsers.map((user, i) => (
               <UserCard key={i} user={user} />
             ))}
